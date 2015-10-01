eab721eaa62a37f71f2141908879a7a83f7bdd25
sharhar <wiishahar@gmail.com>
2015-09-30 19:21:57 -0700

    some basic lwjgl code

--- ludumdaregame/src/code/Main.java
+++ ludumdaregame/src/code/Main.java
@@ -1,10 +1,31 @@
 package code;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
+		try {
+			Display.setDisplayMode(new DisplayMode(800,  600));
+			Display.setTitle("My Window");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		glClearColor(1,0,1,1);
+		
+		while(!Display.isCloseRequested()) {
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			Display.update();
+		}
+		
+		Display.destroy();
 	}
 
 }
