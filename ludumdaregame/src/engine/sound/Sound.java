19e4d9c8c1e143ced1ed0193b864a3d13a1a819a
root <wiishahar@gmail.com>
2015-10-03 11:11:27 -0700

    Created sound class

---
+++ ludumdaregame/src/engine/sound/Sound.java
@@ -0,0 +1,97 @@
+package engine.sound;
+
+import static org.lwjgl.openal.AL10.*;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.util.WaveData;
+import org.lwjgl.util.vector.Vector3f;
+
+public class Sound {
+	
+	int ID = 0;
+	int bufferID = 0;
+	public Vector3f pos;
+	int vol = 0;
+	
+	public static boolean on = true;
+	
+	public boolean playing = false;
+	private boolean started = false;
+	
+	public Sound(String path, Vector3f pos) {
+		this.pos = pos;
+		
+		WaveData data = WaveData.create(path);
+		IntBuffer buffer = BufferUtils.createIntBuffer(1);
+		alGenBuffers(buffer);
+		alBufferData(buffer.get(0), data.format, data.data, data.samplerate);
+		data.dispose();
+		
+		bufferID = buffer.get(0);
+		
+		ID = alGenSources();
+		
+		alSourcei(ID, AL_BUFFER, buffer.get(0));
+		alSource3f(ID, AL_POSITION, pos.x,pos.y,pos.z);
+		alSource3f(ID, AL_VELOCITY, 0,0,0);
+	}
+	
+	public void volUp(float amount) {
+		pos.z += amount;
+		alSource3f(ID, AL_POSITION, pos.x,pos.y,pos.z);
+	}
+	
+	public void volDown(float amount) {
+		pos.z -= amount;
+		alSource3f(ID, AL_POSITION, pos.x,pos.y,pos.z);
+	}
+	
+	public void destroy() {
+		alDeleteSources(ID);
+		alDeleteBuffers(bufferID);
+	}
+	
+	public void play() {
+		if(!on) {
+			return;
+		}
+		alSourcePlay(ID);
+		playing = true;
+		started = true;
+	}
+	
+	public void pause() {
+		if(!on) {
+			return;
+		}
+		alSourcePause(ID);
+		playing = false;
+	}
+	
+	public void resume() {
+		if(!on) {
+			return;
+		}
+		if(!playing && started) {
+			alSourcePlay(ID);
+		}
+		playing = true;
+	}
+	
+	public void stop() {
+		alSourceStop(ID);
+		playing = false;
+		started = false;
+	}
+	
+	public void setLooping(boolean loop) {
+		if(loop) {
+			alSourcei (ID, AL_LOOPING,  AL_TRUE);
+		} else {
+			alSourcei (ID, AL_LOOPING,  AL_FALSE);
+		}
+		
+	}
+}
--- ludumdaregame/src/game/Main.java
+++ ludumdaregame/src/game/Main.java
@@ -1,15 +1,20 @@
 package game;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.openal.AL;
+import org.lwjgl.util.vector.Vector3f;
+
 import engine.Game;
 import engine.graphics.BasicRenderer;
 import engine.graphics.Texture;
 import engine.objects.Sprite;
+import engine.sound.Sound;
 import engine.window.Loop;
 
 public class Main implements Loop{
 	
 	Sprite test;
-	
+	Sound sound;
 	float x = 0;
 	
 	public void run() {
@@ -19,9 +24,16 @@ public class Main implements Loop{
 	
 	public Main() {
 		Game.init("Game Title", 800, 600, this);
+		try {
+			AL.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		test = new Sprite(100, 100, 200, 200, 0, new Texture("/test.png"));
-		
+		sound = new Sound("BM.wav", new Vector3f());
+		sound.play();
 		Game.start();
 	}
 	
