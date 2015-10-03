dbf922069325da64ec6ba032d1cdf3f25c679e73
root <wiishahar@gmail.com>
2015-10-02 20:42:56 -0700

    ability to change between fonts

--- ludumdaregame/src/engine/graphics/font/FontManager.java
+++ ludumdaregame/src/engine/graphics/font/FontManager.java
@@ -5,8 +5,14 @@ import java.awt.Font;
 public class FontManager {
 	
 	public static TrueTypeFont arial = new TrueTypeFont(new Font("Arial",Font.PLAIN, 46), true);
+	public static TrueTypeFont arialBold = new TrueTypeFont(new Font("Arial",Font.BOLD, 46), true);
+	
+	public static TrueTypeFont current = arial;
+	public static void setFont(TrueTypeFont font) {
+		current = font;
+	}
 	
 	public static void drawString(float x, float y, String text, float scale) {
-		arial.drawString(x, y, text, scale, scale);
+		current.drawString(x, y, text, scale, scale);
 	}
 }
